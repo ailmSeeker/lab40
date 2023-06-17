de9908ccdae8655a26c9fd63a4084b73d1942fce
Mohamed Yusuf <study@MohamedYusufs-MacBook-Pro.local>
2023-06-17 13:53:14 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+
+function returnFirstTwoDrivers(x){ return [x[0], x[1]] }
+function returnLastTwoDrivers(x){ return [x[x.length - 2], x[x.length - 1]]}
+
+let selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+
+function createFareMultiplier(x){
+  return () => {
+    let m = 5;
+    return x * m;
+  }
+}
+function fareDoubler(x){
+  return x * 2;
+}
+function fareTripler(x){
+  return x * 3;
+}
+function selectDifferentDrivers(drivers, method){
+  return method(drivers);
+}
